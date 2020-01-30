3107b4517fe1a9000b698d99eaccb45969169f9c
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2020-01-31 02:18:39 +0530

    Revert extended changes

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .models import User
-from .forms import CustomUserCreationForm
-from .forms import CustomUserChangeForm
-
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = User
-    list_display = ['email', 'username',]
+# Register your models here.
--- apps/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-
-from .views import LandingPageView
-
-urlpatterns = [
-    path('', LandingPageView.as_view(), name = 'landing_page'),
-]
